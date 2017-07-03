16b85a683cdf635f34d3d3befacae605fdb230e7
Sergi Carol <sergi@nan-tic.com>
2017-07-03 13:33:02 +0200

    Removed client specific code from the module

--- production.py
+++ production.py
@@ -3,10 +3,6 @@
 from trytond.pool import PoolMeta, Pool
 from trytond.model import fields
 from trytond.pyson import Eval
-import subprocess
-import shutil
-import os
-import tempfile
 
 __all__ = ['Production']
 
@@ -15,8 +11,6 @@ class Production:
     "Production"
     __name__ = 'production'
     __metaclass__ = PoolMeta
-
-    prototype = fields.Function(fields.Binary('Prototype'), 'get_prototype')
 
     sequence = fields.Integer('Sequence')
 
@@ -29,39 +23,3 @@ class Production:
     def order_sequence(tables):
         table, _ = tables[None]
         return [table.sequence == None, table.sequence]
-
-    def get_prototype(self, name=None):
-        '''
-        We should really not be writting the files on the disk.
-        '''
-        pool = Pool()
-        Sale = pool.get('sale.sale')
-
-        if self.origin and not isinstance(self.origin, Sale):
-            return
-        mockup = self.sale.mockups[0].mock_up
-
-        path_name = tempfile.mkdtemp()
-        path = self.to_jpg(mockup, path_name)
-        with open(path, 'rb') as f:
-            buffer_data = f.read()
-            shutil.rmtree(path_name)
-
-        return self.__class__.prototype.cast(buffer_data)  # py3 compatibility
-
-    @staticmethod
-    def to_jpg(pdf_binary, path_name):
-        pdf_data = pdf_binary.data
-        if not pdf_data:
-            return None
-        new_file, _input = tempfile.mkstemp()
-        os.write(new_file, pdf_data)
-
-        _output = path_name + '/' + str(pdf_binary.id) + '.jpg'
-
-        subprocess.call(
-            ["convert", '-quality', '90', '-density', '200x200',
-            '-background', 'white', '-alpha', 'remove',
-                _input + '[0]', _output])
-        os.close(new_file)
-        return _output
