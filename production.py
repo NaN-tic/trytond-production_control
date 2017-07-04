767d7d5f1fd68e232d6647b1f498918ee893a61c
Raimon Esteve <raimon@nan-tic.com>
2017-07-04 13:17:39 +0200

    Review code module

--- production.py
+++ production.py
@@ -1,17 +1,14 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-from trytond.pool import PoolMeta, Pool
+from trytond.pool import PoolMeta
 from trytond.model import fields
-from trytond.pyson import Eval
 
 __all__ = ['Production']
 
 
 class Production:
-    "Production"
-    __name__ = 'production'
     __metaclass__ = PoolMeta
-
+    __name__ = 'production'
     sequence = fields.Integer('Sequence')
 
     @classmethod
--- tests/test_production_control.py
+++ tests/test_production_control.py
@@ -1,33 +1,18 @@
+#!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-# import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-# TODO: Remove if no sceneario needed.
-# from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('production_control')
-
-    def test0005views(self):
-        'Test views'
-        test_view('production_control')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class TestProductionControlCase(ModuleTestCase):
+    'Test Production Control module'
+    module = 'production_control'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_production_control.rst',
-    #        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        TestProductionControlCase))
     return suite
